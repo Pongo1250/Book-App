27bb4cbd4c1270834748eb6c6204446be02fced2
Brett Pongratz <Pongo1250@gmail.com>
2024-05-11 23:04:26 -0500

    adding more api functionality

--- src/main/java/com/example/restservice/BookController.java
+++ src/main/java/com/example/restservice/BookController.java
@@ -1,41 +1,52 @@
 package com.example.restservice;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import com.example.restservice.domain.BookH2Caller;
+import com.example.restservice.domain.BookH2Service;
 import com.example.restservice.model.Book;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin
+@RestController
+public class BookController {
+    BookH2Service h2Service = new BookH2Service();
 
-    @RestController
-    public class BookController {
-        BookH2Caller h2Caller = new BookH2Caller();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-        private static final String template = "Hello, %s!";
-        private final AtomicLong counter = new AtomicLong();
+    @GetMapping("/createBook")
+    public boolean createBook(@RequestParam(value = "title") String title,
+                               @RequestParam(value = "author") String author,
+                               @RequestParam(value = "publicationYear") int publicationYear) {
+        List<Book> books = retrieveAllBooks();
+        int newBookID =  books.size() + 1;
+        Book newBookEntry = new Book(newBookID, title, author, publicationYear);
+        return h2Service.addBook(newBookEntry);
+    }
 
-        @GetMapping("/createBook")
-        public boolean createBook(@RequestParam(value = "title") String title,
-                                   @RequestParam(value = "author") String author,
-                                   @RequestParam(value = "publicationYear") int publicationYear) {
-            List<Book> books = retrieveAllBooks();
-            int newBookID =  books.size() + 1;
-            Book newBookEntry = new Book(newBookID, title, author, publicationYear);
-            return h2Caller.addBook(newBookEntry);
-        }
+    @GetMapping("/retrieveAllBooks")
+    public List<Book> retrieveAllBooks() {
+        return h2Service.retrieveAllBooks();
+    }
+    @GetMapping("/deleteBook")
+    public boolean deleteBook(@RequestParam(value = "id") int id) {
+        return h2Service.deleteBook(id);
+    }
 
-        @GetMapping("/retrieveAllBooks")
-        public List<Book> retrieveAllBooks() {
-            return h2Caller.retrieveAllBooks();
-        }
+    @GetMapping("/updateBook")
+    public boolean updateBook(@RequestParam(value = "id") int id,
+                              @RequestParam(value = "title") String title,
+                              @RequestParam(value = "author") String author,
+                              @RequestParam(value = "publicationYear") int publicationYear) {
+        return h2Service.updateBook(id, title, author, publicationYear);
+    }
 
-        @GetMapping("/retrieveBook")
-        public Book retrieveBook(@RequestParam(value = "id") int id) {
-            return h2Caller.retrieveBook(id);
-        }
+    @GetMapping("/retrieveBook")
+    public Book retrieveBook(@RequestParam(value = "id") int id) {
+        return h2Service.retrieveBook(id);
+    }
 
 }
---
+++ src/main/java/com/example/restservice/domain/BookH2Service.java
@@ -0,0 +1,107 @@
+package com.example.restservice.domain;
+
+import com.example.restservice.model.Book;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookH2Service {
+    String jdbcURL = "jdbc:h2:mem:testdb"; // Connect to an in-memory database named "test"
+    String username = "sa"; // Default username
+    String password = "password"; // Default password (empty)
+
+    public boolean addBook(Book book)  {
+        try (Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+             Statement stmt = conn.createStatement()) {
+            // Create a table
+            createTable(stmt);
+            // Insert a row
+            String insertSql = "INSERT INTO books (id, title, author, publicationYear) VALUES ("+
+                    String.valueOf(book.getId())+", '"+ book.getTitle()+"', '"+book.getAuthor()+"', "+String.valueOf(book.getPublicationYear())+")";
+            stmt.execute(insertSql);
+            return true;
+        } catch (Exception e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+            return false;
+        }
+    }
+
+    public Book retrieveBook(int id) {
+        String retrieveByIDSQL = "SELECT * FROM books WHERE id = " + id+";";
+
+        try (Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+             Statement statement = conn.createStatement();){
+            //create Table
+            createTable(statement);
+            ResultSet resultSet = statement.executeQuery(retrieveByIDSQL);
+            resultSet.next();
+            id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            int publicationYear = resultSet.getInt("publicationYear");
+            return new Book(id, title, author, publicationYear);
+        } catch (Exception e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+            return null;
+        }
+    }
+
+    public List<Book> retrieveAllBooks() {
+        String retrieveAllSql = "SELECT * FROM books;";
+        try (Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+             Statement statement = conn.createStatement();) {
+            // Create a table
+            createTable(statement);
+            ResultSet resultSet = statement.executeQuery(retrieveAllSql);
+            List<Book> books = new ArrayList();
+            while (resultSet.next()) {
+                // Process each row here
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String author = resultSet.getString("author");
+                int publicationYear = resultSet.getInt("publicationYear");
+                books.add(new Book(id, title, author, publicationYear));
+            }
+            return books;
+        } catch (SQLException e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+            return new ArrayList<Book>();
+        }
+    }
+
+    public boolean deleteBook(int id) {
+        String deleteBookSQL = "DELETE FROM books WHERE id = "+id+";";
+        try (Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+             Statement statement = conn.createStatement();) {
+            createTable(statement);
+            statement.execute(deleteBookSQL);
+            return true;
+        } catch (SQLException e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+            return false;
+        }
+    }
+
+    public boolean updateBook(int id, String title, String author, int publicationYear) {
+        String updateBookSQL = "UPDATE books SET title = '"+title+"', author = '"+author+"', publicationYear = " +publicationYear +" WHERE id =" +id + ";";
+        try (Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+             Statement statement = conn.createStatement();) {
+            createTable(statement);
+            statement.executeUpdate(updateBookSQL);
+            return true;
+        } catch (SQLException e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+            return false;
+        }
+    }
+
+    private void createTable(Statement statement) {
+        try {
+            statement.execute("CREATE TABLE IF NOT EXISTS books (id INT PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), publicationYear INT)");
+
+        } catch (SQLException e) {
+            System.out.print(e.getMessage() +" " +e.getStackTrace());
+        }
+    }
+}
